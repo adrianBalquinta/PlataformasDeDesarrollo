bed1fc7f2aaa23b72dcc57ad397532cfa0b69d17
“tuVieja” <adrian.balquinta@davinci.edu.ar>
2020-08-25 21:29:33 -0300

    clase 25-08-2020

---
+++ clase25_08/clase25_08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase25_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
