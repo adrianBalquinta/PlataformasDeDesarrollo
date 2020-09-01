36aeab6fb309ae83836ab164d208478970942916
adrian <adrian.balquinta@davinci.edu.ar>
2020-09-01 20:53:35 -0300

    JUego para adivinar un numero

--- 25-08-2020/App2/Program.cs
+++ 25-08-2020/App2/Program.cs
@@ -6,7 +6,36 @@ namespace App2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             *  Console.WriteLine("Ingrese un numero");
+            string valor = Console.ReadLine();
+            int numero = 1;
+            numero = int.Parse(valor);
+           */
+
+            int eleccion = 0;
+            int numero = 7;
+            int intentos = 1;
+
+            Console.WriteLine("Intente desifrar el número. Tenes 3 intentos");
+            eleccion = int.Parse(Console.ReadLine());
+           
+            while (eleccion != numero && intentos < 3) 
+            {
+                Console.WriteLine("Intente desifrar el número");
+                eleccion = int.Parse(Console.ReadLine());
+                intentos++;
+            }
+
+            if(eleccion == numero)
+            {
+                Console.WriteLine("Adivinaste");
+            }
+            
+
+
+
+
         }
     }
 }
