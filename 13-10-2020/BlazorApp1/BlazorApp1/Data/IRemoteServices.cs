deda6498bf47fb9207901b83c57ef2c563588293
adrian <adrian.balquinta@davinci.edu.ar>
2020-12-02 06:50:19 -0300

    Segundo parcial (arreglo de bugs)

---
+++ 13-10-2020/BlazorApp1/BlazorApp1/Data/IRemoteServices.cs
@@ -0,0 +1,54 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public interface IRemoteServices
+    {
+
+
+
+
+        [Get("/Detalle")]
+        Task<List<Detalle>> GetAllDetalle();
+
+        [Get("/Detalle/{id}")]
+        Task<Detalle> GetDetalle(int id);
+
+        [Post("/Detalle")]
+        Task<Detalle> CrearDetalle(Detalle valor);
+
+
+        [Get("/Recurso")]
+        Task<List<Recurso>> GetAllRecurso();
+
+        [Get("/Recurso/{id}")]
+        Task<Recurso> GetRecurso(int id);
+
+        [Post("/Recurso")]
+        Task<Recurso> CrearRecurso(Recurso valor);
+
+        [Get("/Tarea")]
+        Task<List<Tarea>> GetAllTarea();
+
+        [Get("/Tarea/{id}")]
+        Task<Tarea> GetTarea(int id);
+
+        [Post("/Tarea")]
+        Task<Tarea> CrearTarea(Tarea valor);
+
+
+        [Get("/Usuario")]
+        Task<List<Usuario>> GetAllUsuario();
+
+        [Get("/Usuario/{id}")]
+        Task<Usuario> GetUsuario(int id);
+
+        [Post("/Usuario")]
+        Task<Usuario> GuardarUsuario(Usuario valor);
+
+    }
+}
